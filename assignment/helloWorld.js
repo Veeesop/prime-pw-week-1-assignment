73cf39ea7a531f8f49a6e167fad0ca8e4a2e8249
Alexander Ratanas <alexanderratanas@gmail.com>
2022-03-29 20:24:02 -0500

    add some freshness to hello.js

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,1 +1,6 @@
-console.log('hello world!')
+console.log('hello world!');
+console.log('Now, this is a story all about how');
+console.log('My life got flipped-turned upside down');
+console.log("And I'd like to take a minute");
+console.log('Just sit right there');
+console.log("I'll tell you how I became the prince of a town called Bel-Air");
